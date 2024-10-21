6eb487f137bd811cd1a48dd68c3203ecb7616004
MURARI <tripathimurari599@gmail.com>
2024-10-22 00:33:30 +0530

    my first commit

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -24,19 +24,25 @@ const imageAltText = "purple and blue abstract background";
  * Sort description that expands on your title on the Home component.
  */
 const description =
-  "I'm a UI/UX student studying at Barnett Technical University. I enjoy creating unique and simplistic user interfaces in creative ways.";
+ 
+"Passionate BTech student specializing in web development and software engineering, with a focus on creating responsive and user-friendly applications.";
 
 /**
  * List of some of skills or technologies you work on, are learning,
  * passionate about, or enjoy,
  */
 const skillsList = [
-  "Web design",
-  "User experience",
-  "Inclusive design",
-  "Focus group testing",
-  "Mobile user interfaces",
   "Graphic design",
+  "Responsive web design",
+  "Frontend development",
+  "Data structures & algorithms",
+  "Java programming",
+  "JavaScript (React, Bootstrap)",
+  "Python (Basic knowledge)",
+  "HTML5",
+  "CSS3",
+  "Version control (Git)",
+  "UI/UX prototyping (Figma)",
 ];
 
 /**
@@ -45,7 +51,7 @@ const skillsList = [
  * about you on a professional level.
  */
 const detailOrQuote =
-  "I am passionate about solving problems in new creative ways to drive innovation. By leveraging my UI/UX experience I continually look for new and better ways to make tech accessible by all.";
+ "With a strong foundation in web development and software engineering, I am driven by a passion for building intuitive and responsive applications. My expertise spans React, JavaScript, Bootstrap, and Java, and I am committed to continuously expanding my skill set in areas like machine learning. Whether it's designing sleek web interfaces in Figma or developing efficient, user-centered solutions, I thrive on turning ideas into impactful digital experiences.";
 
 const About = () => {
   return (
--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -19,9 +19,9 @@ import React from "react";
  * Need an image? Check out https://unsplash.com to download a photo you
  * freely use on your site.
  */
-import image from "../images/design-desk.jpeg";
+import image from "../images/tablee.jpg";
 
-const imageAltText = "desktop with books and laptop";
+const imageAltText = "preparing plan on notebook via writing using pen";
 
 /**
  * Project list
@@ -31,28 +31,28 @@ const imageAltText = "desktop with books and laptop";
  */
 const projectList = [
   {
-    title: "10 Things To Know About Azure Static Web Apps 🎉",
+    title: "Movie Loader",
     description:
-      "Collaboration to create a beginner friendly article to help explain Azure Static Web Apps and tooling to get started.",
-    url: "https://dev.to/azure/10-things-to-know-about-azure-static-web-apps-3n4i",
+      "Developed a webpage that allows users to search for movies, view posters, and explore related films. The application uses an API to fetch detailed movie data, including posters and suggestions based on the search query.",
+    url: "https://murarimohantripathi.github.io/Movie_Loader/",
   },
   {
-    title: "Web Development for Beginners",
+    title: "TextUtils",
     description:
-      "Contributed sketch note imagery to accompany each lesson. These help provide visual representation of what is being taught.",
-    url: "https://github.com/microsoft/web-dev-for-beginners",
+      "A React-based webpage that allows users to perform various text operations, such as removing extra spaces, converting text to uppercase or lowercase, and utilizing a dark mode feature for improved readability.",
+    url: "https://murarimohantripathi.github.io/textutils/",
   },
   {
-    title: "My Resume Site",
+    title: "Food Ordering Website",
     description:
-      "Created from Microsoft's resume workshop and deployed to GitHub pages. Includes my experience and design abilities.",
+      "Developed a simple food ordering platform using HTML, CSS, and JavaScript",
     url: "https://github.com/microsoft/workshop-library/tree/main/full/build-resume-website",
   },
   {
-    title: "GitHub Codespaces and github.dev",
+    title: "Tic Tac Toe",
     description:
-      "Video interview to explain when to use GitHub.dev versus GitHub Codespaces, and how best to use each tool.",
-    url: "https://www.youtube.com/watch?v=c3hHhRME_XI",
+      "build a Tic Tac Toe game using React js ",
+    url: "https://murarimohantripathi.github.io/TicTacToe/",
   },
 ];
 
